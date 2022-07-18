e1e3f340acf910c3e10671cfbc5e6e533a37e5eb
joo <yukota.yuki@hotmail.com>
2022-07-18 13:43:51 +0000

    first commit

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "joo's dotfiles";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, ... }@inputs: with inputs;
+    let
+      #username = builtins.getEnv "USER";
+      username = "joo";
+
+      # system - argument
+      getHomeDirectory = system: with nixpkgs.legacyPackages.${system}.stdenv;
+        if isDarwin then
+          "/Users/${username}"
+        else if isLinux then
+          "/home/${username}"
+          #"/${username}"
+        else "";
+      
+      mkHomeConfig = args: home-manager.lib.homeManagerConfiguration {
+        pkgs = import nixpkgs {
+          inherit (args) system;
+          config = {
+            allowUnfree = true;
+            allowUnsupportedSystem = true;
+          };
+        };
+        modules = [
+          ./home.nix
+          {
+            home = {
+              inherit username;
+              homeDirectory = getHomeDirectory args.system;
+              stateVersion = "22.05";
+            };
+          }
+        ];
+      };
+    in
+    {
+      homeConfigurations."${username}" = mkHomeConfig {
+        system = "aarch64-linux";
+      };
+    };
+}
---
+++ home.nix
@@ -0,0 +1,34 @@
+{ config, pkgs, lib, ... }:
+
+let
+  inherit (pkgs.stdenv) isDarwin isLinux;
+
+in
+{
+  home.packages = with pkgs; [
+    jq
+  ] ++ lib.lists.optionals isLinux [
+    htop
+  ];
+
+  nix = {
+    package = pkgs.nixUnstable;
+    settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+    };
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager = {
+    enable = true;
+  };
+  programs.vim = {
+    enable = true;
+  };
+  programs.direnv = {
+    enable = true;
+  };
+  programs.zsh = {
+    enable = true;
+  };
+}
