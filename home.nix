a786fa25cd86dc1ab15e333c2a7bd12749d25438
joo <yukota.yuki@hotmail.com>
2022-08-30 22:52:18 +0900

    feat(autokey): add setting to register autokey in autostart

--- home.nix
+++ home.nix
@@ -40,6 +40,7 @@ in
   xdg.configFile."nix/nix.conf".source = ./nix.conf;
   xdg.configFile."nixpkgs/config.nix".source = ./nixpkgs-config.nix;
   xdg.configFile."autokey".source = config.lib.file.mkOutOfStoreSymlink "${dotDir}/.config/autokey";
+  xdg.configFile."autostart/autokey.desktop".source = config.lib.file.mkOutOfStoreSymlink "${dotDir}/.config/autostart/autokey.desktop";
 
   fonts.fontconfig.enable = true;
 
