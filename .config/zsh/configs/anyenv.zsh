f2d1333d18d14afbb1185bb44efa9fc5077801fe
joo <yukota.yuki@hotmail.com>
2022-09-02 11:14:42 +0900

    fix: update zsh config

--- .config/zsh/configs/anyenv.zsh
+++ .config/zsh/configs/anyenv.zsh
@@ -1,5 +1,7 @@
 # asdf
-# source $HOME/.asdf/asdf.sh
+if [[ $DISTRI != 'nixos' ]]; then
+    source $HOME/.asdf/asdf.sh
+fi
 
 # # nix for mac
 # export NIX_PATH="$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}"
