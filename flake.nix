fdbc7e44c066ec6eb92bec0d5d3983ff0b42ce2f
joo <yukota.yuki@hotmail.com>
2022-09-04 22:48:26 +0900

    fix: improve mkHomeConfig

--- flake.nix
+++ flake.nix
@@ -19,29 +19,45 @@
       #username = builtins.getEnv "USER";
       username = "joo";
 
-      # system - argument
-      getHomeDirectory = system: with nixpkgs.legacyPackages.${system}.stdenv;
-        if isDarwin then
-          "/Users/${username}"
-        else if isLinux then
-          "/home/${username}"
-          #"/${username}"
-        else "";
-      
-      mkHomeConfig = args: home-manager.lib.homeManagerConfiguration {
-        pkgs = import nixpkgs {
-          inherit (args) system;
-          config = {
-            allowUnfree = true;
-            allowUnsupportedSystem = true;
-          };
+      # # system - argument
+      # getHomeDirectory = system: with nixpkgs.legacyPackages.${system}.stdenv;
+      # if isDarwin then
+      #   "/Users/${username}"
+      # else if isLinux then
+      #   "/home/${username}"
+      #   #"/${username}"
+      # else "";
+
+      mkHomeConfig = 
+      let
+        homeDirectoryPrefix = pkgs:
+          if pkgs.stdenv.hostPlatform.isDarwin then "/Users" else "/home";
+      in
+      { system ? "x86_64-linux"
+      , pkgs ? (self.lib.pkgsForSystem {
+        inherit sytem; 
+        config = {
+          allowUnfree = true;
+          allowUnsupportedSystem = true;
         };
+      })
+      , homeDirectory ? "${homeDirectoryPrefix pkgs}/${username}"
+      }:
+      home-manager.lib.homeManagerConfiguration {
+        # pkgs = import nixpkgs {
+        #   inherit system;
+        #   config = {
+        #     allowUnfree = true;
+        #     allowUnsupportedSystem = true;
+        #   };
+        # };
         modules = [
           ./home.nix
           {
             home = {
               inherit username;
-              homeDirectory = getHomeDirectory args.system;
+              inherit homeDirectory;
+              # homeDirectory = getHomeDirectory args.system;
               # stateVersion = "22.05";
             };
           }
@@ -89,9 +105,7 @@
         "hm-linux@arm" = mkHomeConfig {
           system = "aarch64-linux";
         };
-        hm-linux = mkHomeConfig {
-          system = "x86_64-linux";
-        };
+        hm-linux = mkHomeConfig {};
       };
 
       nixosConfigurations = {
