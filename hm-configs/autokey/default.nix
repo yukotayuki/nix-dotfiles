297d57df27209f0be6a73abe64a7392e11f1ef3e
joo <yukota.yuki@hotmail.com>
2022-09-11 16:40:21 +0900

    fix: refactoring autokey settings

--- hm-configs/autokey/default.nix
+++ hm-configs/autokey/default.nix
@@ -7,7 +7,16 @@ in lib.mkIf (isLinux)
 {
   home.packages = with pkgs; [ autokey ];
   xdg.configFile = {
-    "autokey".source = config.lib.file.mkOutOfStoreSymlink "${dotDir}/.config/autokey";
-    "autostart/autokey.desktop".source = config.lib.file.mkOutOfStoreSymlink "${dotDir}/.config/autostart/autokey.desktop";
+    "autokey" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${dotDir}/.config/autokey";
+    };
+
+    "autostart/autokey.desktop" =
+    let
+      autokey_desktop = "${dotDir}/.config/autostart/autokey.desktop";
+    in
+    {
+      source = config.lib.file.mkOutOfStoreSymlink "${autokey_desktop}";
+    };
   };
 }
