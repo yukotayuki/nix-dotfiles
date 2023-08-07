b8c9edfa24995e9a210efbafd873c42605dfa7b3
joo <yukota.yuki@hotmail.com>
2023-08-07 23:13:48 +0900

    feat: modify autokey config to also support Tilix

--- .config/autokey/data/my-settings/global/end_ctrl-e.py
+++ .config/autokey/data/my-settings/global/end_ctrl-e.py
@@ -1,7 +1,7 @@
 # Enter script code
 winClass = window.get_active_class()
 
-if "terminal" in winClass or "wezterm" in winClass or "Alacritty" in winClass:
+if "terminal" in winClass or "wezterm" in winClass or "Alacritty" in winClass or "Tilix" in winClass:
     keyboard.send_keys("<ctrl>+e")
 else:
     keyboard.send_keys("<end>")
