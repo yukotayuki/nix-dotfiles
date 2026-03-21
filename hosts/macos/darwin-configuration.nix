d788468be2d80b9d606ebe01c8d7fafe7be74fa5
joo <yukota.yuki@hotmail.com>
2026-03-22 00:01:38 +0900

    fix(darwin): homebrew cleanup の値を "uninstall" に修正
    "uninstalled" は無効な値。有効な値は none / check / uninstall / zap。

--- hosts/macos/darwin-configuration.nix
+++ hosts/macos/darwin-configuration.nix
@@ -9,9 +9,9 @@
 
   homebrew = {
     enable = true;
-    # "zap" にすると宣言にない既存パッケージが全部消えるため "uninstalled" にとどめる。
+    # "zap" にすると宣言にない既存パッケージが全部消えるため "uninstall" にとどめる。
     # 宣言から外したパッケージは次回 darwin-switch 時にアンインストールされる。
-    onActivation.cleanup = "uninstalled";
+    onActivation.cleanup = "uninstall";
     brews = [
       # telnet: nixpkgs の inetutils は Darwin 向けビルドが不安定なため homebrew で管理
       "telnet"
