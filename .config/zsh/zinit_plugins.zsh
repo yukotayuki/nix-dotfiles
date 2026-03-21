0c7decb272e0a8e6c5fb6137832488061a012cd7
joo <yukota.yuki@hotmail.com>
2026-03-22 00:37:04 +0900

    refactor(zsh): 設定ファイルをルートから .config/zsh/ に集約
    .zshrc / .zshenv をルートに置く理由がないため .config/zsh/ に移動。
    zinit_plugins.zsh は .zshrc に統合して別ファイル不要に。
    functions/ssh.zsh は iTerm2 専用のプロファイル切り替えで Ghostty 移行後は不要。
    direnv hook は home-manager の programs.direnv.enable で管理済みのため削除。
    プロンプトの ($(arch)) は Apple Silicon 専用環境では常に arm64 のため削除。

--- .config/zsh/zinit_plugins.zsh
+++
@@ -1,7 +0,0 @@
-# 構文ハイライト
-zinit light zsh-users/zsh-syntax-highlighting
-zinit light chrissicool/zsh-256color
-
-# 補完機能
-zinit light zsh-users/zsh-completions
-zinit light zsh-users/zsh-autosuggestions
